8146e761f9b6f7e2045c35740954be874f350dc2
ch-ratnaji <ratnaji.it91@gmail.com>
2025-09-22 18:04:54 +0530

    iterated the course goals through map method and used view element to have proper styling in  ios.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
@@ -10,6 +11,10 @@ export default function App() {
 
   function addGoalHandler() {
     console.log(enteredGoalText);
+    setCourseGoals((courseGoals) => {
+      return [...courseGoals, enteredGoalText];
+    });
+    setEnteredGoalText("");
   }
 
   return (
@@ -24,7 +29,11 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {courseGoals.map((goal) => (
+          <View key={goal} style={styles.goalItem}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -54,5 +63,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    color: "white",
   },
 });
